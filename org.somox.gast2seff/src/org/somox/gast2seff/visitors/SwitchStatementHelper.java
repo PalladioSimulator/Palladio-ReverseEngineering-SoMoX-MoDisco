1c9ee16f062c50a58323a955317f75185de51865
Steffen Becker <sbecker@ipd.uka.de>
2015-09-28 20:38:06 +0000

    fixed an array out of bounds access
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/SoMoX/Core/trunk@29223 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.somox.gast2seff/src/org/somox/gast2seff/visitors/SwitchStatementHelper.java
+++ org.somox.gast2seff/src/org/somox/gast2seff/visitors/SwitchStatementHelper.java
@@ -11,54 +11,55 @@ import org.eclipse.gmt.modisco.java.SwitchStatement;
  * Computes for a {@link SwitchStatement} the case branches in a way that to the
  * case branches, that do not end with break, the following case branch is
  * added.
- * 
+ *
  * @author Oliver
- * 
+ *
  */
 public class SwitchStatementHelper {
 
-	public static ArrayList<ArrayList<Statement>> createBlockListFromSwitchStatement(
-			SwitchStatement switchStatement) {
-		ArrayList<ArrayList<Statement>> blockList = new ArrayList<ArrayList<Statement>>();
+    public static ArrayList<ArrayList<Statement>> createBlockListFromSwitchStatement(
+            final SwitchStatement switchStatement) {
+        final ArrayList<ArrayList<Statement>> blockList = new ArrayList<ArrayList<Statement>>();
 
-		for (int actStatementNo = 0; actStatementNo < switchStatement.getStatements().size(); actStatementNo++) {
+        for (int actStatementNo = 0; actStatementNo < switchStatement.getStatements().size(); actStatementNo++) {
 
-			Statement statement = switchStatement.getStatements().get(actStatementNo);
-			if (statement instanceof SwitchCase) {
-				int currentPointer = actStatementNo;
-				ArrayList<Statement> block = new ArrayList<Statement>();
+            final Statement statement = switchStatement.getStatements().get(actStatementNo);
+            if (statement instanceof SwitchCase) {
+                final int currentPointer = actStatementNo;
+                final ArrayList<Statement> block = new ArrayList<Statement>();
 
-				while (true) {
-					// if last statement
-					if (actStatementNo == switchStatement.getStatements().size() - 1) {
-						Statement lastStatement = switchStatement.getStatements().get(actStatementNo); 
-						if (lastStatement instanceof BreakStatement | lastStatement instanceof SwitchCase) {
-							break;
-						} else{
-							block.add(lastStatement);
-						}
-						
-					}
-					//goto next statement
-					actStatementNo++;
-					Statement nextStatement = switchStatement.getStatements()
-							.get(actStatementNo);
-					if (nextStatement instanceof SwitchCase) {
-						continue;
-					}
-					//if the next Statement is not break, then add it
-					if (!(nextStatement instanceof BreakStatement )) {
-						block.add(nextStatement);
-					} else {
-						break;
-					}
-				}
-				
-				blockList.add(block);
-				actStatementNo = currentPointer;
-			}
-		}
-		return blockList;
-	}
+                while (true) {
+                    // if last statement
+                    if (actStatementNo == switchStatement.getStatements().size() - 1) {
+                        final Statement lastStatement = switchStatement.getStatements().get(actStatementNo);
+                        if (lastStatement instanceof BreakStatement | lastStatement instanceof SwitchCase) {
+                            break;
+                        } else{
+                            block.add(lastStatement);
+                            break;
+                        }
+
+                    }
+                    //goto next statement
+                    actStatementNo++;
+                    final Statement nextStatement = switchStatement.getStatements()
+                            .get(actStatementNo);
+                    if (nextStatement instanceof SwitchCase) {
+                        continue;
+                    }
+                    //if the next Statement is not break, then add it
+                    if (!(nextStatement instanceof BreakStatement )) {
+                        block.add(nextStatement);
+                    } else {
+                        break;
+                    }
+                }
+
+                blockList.add(block);
+                actStatementNo = currentPointer;
+            }
+        }
+        return blockList;
+    }
 
 }
